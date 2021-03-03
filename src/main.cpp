eb3ab4beeed04559a8daea13e6e7cfec313a06e8
guttih <gudjonholm@gmail.com>
2021-03-03 13:02:17 +0000

    fixing calcuation

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,59 @@
 #include <Arduino.h>
 
-
 #include "servo.h"
 
-  Servo towerProSg90(12,  180, 0, 500, 2400, "sg90");
-  Servo towerProMg995(13, 120, 1, "mg995");
+Servo towerProSg90(12, 180, 0, 500, 2400, "sg90");
+Servo towerProMg995(13, 120, 1, "mg995");
 
 const int ledPin = 2;
 
-
-void setup() 
+void setup()
 {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   Serial.begin(115200);
-  delay(1000); 
+  delay(1000);
   towerProSg90.setAngle(0);
   towerProMg995.setAngle(0);
   digitalWrite(ledPin, LOW);
-  Serial.println("starting");
+  Serial.println("   ---------------------------------------------------- ");
+  Serial.println("  | If you type the following numbers and press return |");
+  Serial.println("  | you will give the servos the following commands.   |");
+  Serial.println("   ---------------------------------------------------- ");
+  Serial.println();
+  Serial.println();
+  Serial.println("     ----------------------------------------------- ");
+  Serial.println("    |  Number  Command                              |");
+  Serial.println("    | 0 - 360  change servo angle to n°             |");
+  Serial.println("    |    1000  Print all servo properties           |");
+  Serial.println("    |    2000  Disable servo and enable idle mode.  |");
+  Serial.println("    |    2001  Enable servo and disable idle mode.  |");
+  Serial.println("     ----------------------------------------------- ");
 }
 
-
-void loop() 
+void loop()
 {
-  if (Serial.available()>0)
-  {      
-    int angle=Serial.parseInt();
+  if (Serial.available() > 0)
+  {
+    int angle = Serial.parseInt();
     Serial.read();
     Serial.println(angle);
-    towerProSg90.setAngle(angle);
-    towerProMg995.setAngle(angle);
+    if (angle == 2000 || angle == 2001)
+    {
+      bool idleOn = (angle == 2000);
+      Serial.println(idleOn ? "Setting servos to Idle" : "Activating servos");
+      towerProSg90.setIdle(idleOn);
+      towerProMg995.setIdle(idleOn);
+    }
+    else if (angle == 1000)
+    {
+      towerProSg90.serialPrintInfo();
+      towerProMg995.serialPrintInfo();
+    }
+    else
+    {
+      towerProSg90.setAngle(angle);
+      towerProMg995.setAngle(angle);
+    }
   }
-  
 }
--- src/servo.cpp
+++ src/servo.cpp
@@ -9,7 +9,8 @@
  */
 Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel)
 {
-  init(pin, maxAngle, channel);
+  //use the defaults
+  init(pin, maxAngle, channel, _minUs, _maxUs, _name);
 }
 
 /**
@@ -20,22 +21,17 @@ Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel)
  * @param pin 
  * @param maxAngle 
  * @param channel 
- * @param strName 
+ * @param name 
  */
-Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel, const char *strName)
+Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel, const char *name)
 {
-  _name = strName;
-  init(pin, maxAngle, channel);
-  setDebugMode(true);
+  //use the defaults
+  init(pin, maxAngle, channel, _minUs, _maxUs, name);
 }
 
-Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel, uint16_t minUs, uint16_t maxUs, const char *strName)
+Servo::Servo(uint16_t pin, uint16_t maxAngle, uint16_t channel, uint16_t minUs, uint16_t maxUs, const char *name)
 {
-  _name = strName;
-  _minUs = minUs;
-  _maxUs = maxUs;
-  init(pin, maxAngle, channel);
-  setDebugMode(true);
+  init(pin, maxAngle, channel, minUs, maxUs, name);
 }
 
 void Servo::setDebugMode(bool activate)
@@ -43,13 +39,16 @@ void Servo::setDebugMode(bool activate)
   _debugging = activate;
 }
 
-void Servo::init(uint16_t pin, uint16_t maxAngle, uint16_t channel)
+void Servo::init(uint16_t pin, uint16_t maxAngle, uint16_t channel, uint16_t minUs, uint16_t maxUs, const char *name)
 {
+  _angle = 0;
   _pin = pin;
   _maxAngle = maxAngle;
   _channel = channel;
-  _angle = 0;
-  setDebugMode(false);
+  _minUs = minUs;
+  _maxUs = maxUs;
+  _name = name;
+  setDebugMode(_name != NULL);
   pinMode(_pin, OUTPUT);
   ledcAttachPin(_pin, _channel);
   ledcSetup(_channel, 50, 16);
@@ -60,6 +59,8 @@ void Servo::setAngle(uint16_t angle)
   _angle = min(angle, _maxAngle);
   if (_debugging)
   {
+    if (_isIdle)
+      Serial.print("Servo is set to Idle, ");
     if (_name)
       Serial.print(_name);
 
@@ -70,10 +71,55 @@ void Servo::setAngle(uint16_t angle)
   ledcWrite(_channel, angleToDutyCycle(_angle));
 }
 
+int Servo::angleToOnPeriod(uint16_t angle)
+{
+  float tickLength = (_maxUs - _minUs) / (float)_maxAngle;
+  int us = _minUs + ((angle * tickLength) + 0.5); //+0.5 for rounding
+  return us;
+}
 int Servo::angleToDutyCycle(uint16_t angle)
 {
-  float tickLength = (_maxUs - _minUs) / _maxAngle;
-  int us = _minUs + (angle * tickLength);
+  if (_isIdle)
+    return 0; // The servo motor has been set to idle.
+
+  int us = angleToOnPeriod(angle);
   int duty = (65535 * us) / _periodUs;
   return duty;
+}
+
+void Servo::setIdle(bool idle)
+{
+  _isIdle = idle;
+  if (_debugging)
+  {
+    if (_name)
+      Serial.print(String(_name) + ": ");
+    Serial.print("Setting idle to ");
+    Serial.println(_isIdle);
+  }
+
+  if (_isIdle)
+  {
+    ledcWrite(_channel, 0);
+  }
+  else
+    setAngle(_angle);
+};
+void Servo::serialPrintInfo()
+{
+  Serial.println("-------------------");
+  if (_name)
+    Serial.println(String(" name     : ") + _name);
+  Serial.println(String(" pin       : ") + _pin);
+  Serial.println(String(" channel   : ") + _channel);
+  Serial.println(String(" angle     : ") + _angle);
+  Serial.println(String(" dutyCycle : ") + angleToDutyCycle(_angle));
+  Serial.println(String(" onPeriodUs: ") + angleToOnPeriod(_angle));
+  Serial.println(String(" maxAngle  : ") + _maxAngle);
+  Serial.println(String(" minUs     : ") + _minUs);
+  Serial.println(String(" maxUs     : ") + _maxUs);
+  Serial.println(String(" periodUs  : ") + _periodUs);
+  Serial.println(String(" isIdle    : ") + (_isIdle ? "true" : "false"));
+  Serial.println(String(" debugging : ") + (_debugging ? "true" : "false"));
+  Serial.println("-------------------");
 }
