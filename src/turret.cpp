27018f483680a9a4fa917f47204f696c997a6108
guttih <gudjonholm@gmail.com>
2021-03-04 23:44:46 +0000

    now we can start on the turret

---
+++ src/turret.cpp
@@ -0,0 +1,5 @@
+#include "turret.h"
+
+
+
+Turret::Turret() {};
---
+++ src/turret.h
@@ -0,0 +1,16 @@
+#ifndef TURRET_H
+#define TURRET_H
+#include "arduino.h"
+
+
+class Turret
+{
+private:
+   uint16_t _pin;
+
+public:
+    Turret();
+
+};
+
+#endif
