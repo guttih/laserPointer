0131e9dbdd3e0b2ea9f6b1019b1cff9334d94cc3
guttih <gudjonholm@gmail.com>
2022-09-11 14:30:04 +0000

    ot-fix

--- src/upload-ota.h
+++ src/upload-ota.h
@@ -47,8 +47,8 @@ void setupArduinoOTA(const char *hostName) {
       else if (error == OTA_END_ERROR) Serial.println("End Failed");
     });
 
+  ArduinoOTA.setPort(3232);
   ArduinoOTA.begin();
-  ArduinoOTA.setHostname(hostName);
 }
 
 #endif
